79bd4cd5906f5e1156bb9038b4c7023d784b6990
thompsonsed <thompsonsed@gmail.com>
2019-10-02 17:00:20 +0100

    Fixed typo in windows build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,10 @@
 
-mkdir ../temp
+mkdir ..\temp
+xcopy * src\Python\
 xcopy /S src\Python ..\temp\
 cd ..\
 RD /S /Q somoclu\
 mkdir somoclu
 xcopy /S temp somoclu\
+cd somoclu
 python setup.py install --single-version-externally-managed --record record.txt
