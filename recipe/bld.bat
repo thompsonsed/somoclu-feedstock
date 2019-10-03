89dfc24da4726c380e90a3537b3981a510e64816
thompsonsed <thompsonsed@gmail.com>
2019-10-03 11:38:04 +0100

    Fixed windows build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,8 @@
-
-mkdir ..\temp
-xcopy * src\Python\
-xcopy /S src\Python ..\temp\
-cd ..\
-RD /S /Q somoclu\
-mkdir somoclu
-xcopy /S temp somoclu\
-cd somoclu
-python setup.py install --single-version-externally-managed --record record.txt
+dir
+mkdir "%SRC_DIR%"\src\Python\src
+xcopy /Y "%SRC_DIR%"\* "%SRC_DIR%"\src\Python\src\
+xcopy /S /Y "%SRC_DIR%"\src\Python "%SRC_DIR%"\
+rd /s  /q "%SRC_DIR%"\src
+rd /s  /q "%SRC_DIR%"\data
+dir
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
